7598480406037e917fbf83d5940a618796835c1d
krisztian kaposi <krisztiankaposi@hotmail.com>
2023-04-19 19:28:05 +0100

    number2

--- pages/index.js
+++ pages/index.js
@@ -16,14 +16,23 @@ export default function Home() {
   const [city,setCity]=useState('');
   const [weather,setWeather]=useState({});
   const [loading,setLoading]=useState(false);
-  const url = `https://api.openweathermap.org/data/2.5/weather?q=london&appid=${process.env.NEXT_PUBLIC_WEATHER_KEY}`
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=london&units=imperial&appid=${process.env.NEXT_PUBLIC_WEATHER_KEY}`
 
-
+const fetchWeather = (e)=> {
+  e.preventDefault()
+  setLoading(true)
+  axios.get(url).then((response) => {
+    setWeather(response.data)
+    console.log(response.data)
+  })
+  setCity('')
+  setLoading(false)
+}
   return (
     <main className="">
       <title>MaryRainyPoppins</title>
 
-      <h1>hello00000</h1>
+      <Image src='https://images.unsplash.com/photo-1491002052546-bf38f186af56?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1208&q=80' layout='fill'/>
     </main>
   )
 }
